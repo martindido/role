e724ad1b22e8a3118c99916832691d724b3088b6
Martin Di Domenico <martindido87@gmail.com>
2018-10-23 12:21:16 -0300

    Initial cleanup

--- src/index.js
+++ src/index.js
@@ -11,9 +11,5 @@ const root: ?Element = document.getElementById('root');
 
 if (root) {
     ReactDOM.render(<App/>, root);
-
-    // If you want your app to work offline and load faster, you can change
-    // unregister() to register() below. Note this comes with some pitfalls.
-    // Learn more about service workers: http://bit.ly/CRA-PWA
     serviceWorker.register();
 }
