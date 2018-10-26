67aa6766a5a46bd8d269916b761f743de8b0dcd7
Martin Di Domenico <martindido87@gmail.com>
2018-10-26 00:29:17 -0300

    Removed counter delay

--- src/App/epics/counter.js
+++ src/App/epics/counter.js
@@ -4,7 +4,7 @@ import {
 } from '../constants/actions';
 import { completeUpdate } from '../actions/counter';
 import { ofType } from 'redux-observable';
-import { delay, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 import type { IncrementAction, DecrementAction } from '../types/Action';
 import type { ActionsObservable } from 'redux-observable';
@@ -12,14 +12,12 @@ import type { ActionsObservable } from 'redux-observable';
 export const increment = (action$: ActionsObservable<IncrementAction>) =>
     action$.pipe(
         ofType(INCREMENT_COUNTER),
-        delay(5000),
         map((action: IncrementAction) => completeUpdate(action.payload))
     );
 
 export const decrement = (action$: ActionsObservable<DecrementAction>) =>
     action$.pipe(
         ofType(DECREMENT_COUNTER),
-        delay(5000),
         map((action: DecrementAction) => completeUpdate(-action.payload))
     );
 
