a7f9ca02506734d89f659777c850c4b87057a787
Martin Di Domenico <martindido87@gmail.com>
2018-11-05 23:22:48 -0300

    Improved loading

--- src/App/components/Loading.js
+++ src/App/components/Loading.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import Page from "../containers/Page";
+import Page from '../containers/Page';
+import * as Spinners from 'react-epic-spinners';
+import { Grid } from 'semantic-ui-react';
 
-export default function() {
+import '../styles/Loading.css';
+
+import type { LoadingProps } from 'react-loadable';
+
+const keys = Object.keys(Spinners);
+const randomKey = keys[Math.floor(Math.random() * keys.length)];
+const RandomSpinner = Spinners[randomKey];
+
+export default function(props: LoadingProps) {
+    if (!props.pastDelay) {
+        return null;
+    }
     return (
-        <Page id="Loading" className="loading" title="Loading..." description="This is about really cool stuff." />
+        <Page id="Loading" className="loading" title="Loading..." description="This is about really cool stuff.">
+            <Grid verticalAlign='middle'>
+                <Grid.Column>
+                    <RandomSpinner className='spinner' color='white' size={ 200 }/>
+                </Grid.Column>
+            </Grid>
+        </Page>
     );
 }
